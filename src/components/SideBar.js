c0632558fcbada0283cf7f13221b15808072b268
oscar Kioge <mainoscar94@gmail.com>
2021-01-25 10:13:35 +0300

    done with the sidebar

---
+++ src/components/SideBar.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Button } from '@material-ui/core';
+import './SideBar.css'
+import AddIcon from "@material-ui/icons/Add";
+import InboxIcon from '@material-ui/icons/Inbox';
+import SideBarOptions from './SideBarOptions'
+import StarIcon from '@material-ui/icons/Star';
+import { LabelImportant } from '@material-ui/icons';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import LabelImportantIcon from '@material-ui/icons/LabelImportant';
+import NearMeIcon from '@material-ui/icons/NearMe';
+import NoteIcon from '@material-ui/icons/Note';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { IconButton } from '@material-ui/core';
+import PersonIcon from '@material-ui/icons/Person';
+import DuoIcon from '@material-ui/icons/Duo';
+import PhoneIcon from '@material-ui/icons/Phone';
+function SideBar() {
+    return (
+        <div className='sidebar'>
+            <Button 
+            className='sidebar__compose'
+            startIcon={<AddIcon fontSize="large"/>
+            
+            }>Compose</Button>
+            <SideBarOptions Icon={InboxIcon} title='inbox' number={54} selected={true}/>
+            <SideBarOptions Icon={AccessTimeIcon} title='snoozed' number={54}/>
+            <SideBarOptions Icon={LabelImportantIcon} title='important' number={54}/>
+            <SideBarOptions Icon={NearMeIcon} title='Sent' number={54}/>
+            <SideBarOptions Icon={NoteIcon} title='Drafts' number={54}/>
+            <SideBarOptions Icon={ExpandMoreIcon} title='More' number={54}/>
+            <div className='sidebar__footer'>
+                <div className='sidebar__footerIcons'>
+                    <IconButton>
+                        <PersonIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <DuoIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <PhoneIcon/>
+                    </IconButton>
+                </div>
+            </div>
+            
+            
+        </div>
+    )
+}
+
+export default SideBar
---
+++ src/components/SideBarOptions.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './SideBarOptions.css'
+
+function SideBarOptions({Icon, title, number, selected}) {
+    return (
+        <div className={`sidebarOptions ${selected && 'sidebarOptions--active'}`}>
+            <Icon/>
+            <h3>{title}</h3>
+            <p>{number}</p> 
+        </div>
+    )
+}
+
+export default SideBarOptions
