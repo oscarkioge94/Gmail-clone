a5274c601728e96608d99219c683105ae9e7670e
oscar Kioge <mainoscar94@gmail.com>
2021-01-26 10:54:17 +0300

    done with react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import SideBar from './components/SideBar';;
+import SideBar from './components/SideBar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+    <div className="app">
       <Header/>
+      <div className="app__body">
       <SideBar/>
+      <Switch>
+        <Route path="/mail">
+          <Mail/>
+        </Route>
+        <Route path="/">
+          <EmailList/>
+        </Route>
+      </Switch>
+      </div>
+      
     </div>
+    </Router>
   );
 }
 
