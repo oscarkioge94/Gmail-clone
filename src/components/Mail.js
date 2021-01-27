07f70995b256eebecb864df367beb8c1ea113a49
oscar Kioge <mainoscar94@gmail.com>
2021-01-27 12:36:09 +0300

    implimented the history hook from react router

--- src/components/Mail.js
+++ src/components/Mail.js
@@ -1,10 +1,44 @@
 import React from 'react'
 import './Mail.css'
-
+import { IconButton } from '@material-ui/core';
+import { useHistory } from "react-router-dom"
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import MoveToInboxIcon from '@material-ui/icons/MoveToInbox';
+import ErrorIcon from '@material-ui/icons/Error';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EmailIcon from '@material-ui/icons/Email';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 function Mail() {
+    const history = useHistory();
     return (
         <div className='mail'>
-            <h1>im rocking</h1>
+            <div className='mail__tools'>
+                <div className="mail__toolsLeft">
+                    <IconButton onClick={()=> history.push('/')}>
+                        <ArrowBackIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <MoveToInboxIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <ErrorIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <DeleteIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <EmailIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <WatchLaterIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon/>
+                    </IconButton>
+                </div>
+                <div className="mail__toolsRight"></div>
+            </div>
             
         </div>
     )
