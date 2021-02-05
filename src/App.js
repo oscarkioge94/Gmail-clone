c30ee48579d9ed66d9a6b2f07176cbd948387474
oscar Kioge <mainoscar94@gmail.com>
2021-02-05 10:38:54 +0300

    done with google authentication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import { React, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import SideBar from './components/SideBar';
 import Mail from './components/Mail';
 import EmailList from './components/EmailList';
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { selectSendMessageIsOpen } from "./features/mailSlice"
+import { login, selectUser } from "./features/userSlice"
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,11 +14,31 @@ import {
   Link
 } from "react-router-dom";
 import SendMail from './components/SendMail';
+import Login from './components/Login';
+import { auth } from "./firebase"
 
 function App() {
   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen)
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      if(user){
+        // user logged in
+        dispatch(login({
+                displayName: user.displayName,
+                email:user.email,
+                photoUrl: user.photoURL
+        }))
+      }
+    })
+  },[])
   return (
     <Router>
+      {!user ? (
+        <Login/>
+      ):
     <div className="app">
       <Header/>
       <div className="app__body">
@@ -33,6 +54,7 @@ function App() {
       </div>
      { sendMessageIsOpen && <SendMail/>} 
     </div>
+}
     </Router>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,36 +7,47 @@ import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import AppsIcon from '@material-ui/icons/Apps';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import Avatar from '@material-ui/core/Avatar';
+import { useSelector, useDispatch } from 'react-redux'
+import { selectUser, logout } from "../features/userSlice"
+import { auth } from "../firebase"
 
 function Header() {
+    const user = useSelector(selectUser)
+    const dispatch = useDispatch();
+    const signOut = () => {
+        auth.signOut().then(() =>{
+            dispatch(logout());
+        })
+        
+    }
     return (
         <div className="header">
             <div className="header__left">
                 <IconButton>
-                  <MenuIcon/>
+                    <MenuIcon />
                 </IconButton>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEMmBA2kQGJt8ApVm5zWD_rZFJeFu3T2zH0w&usqp=CAU" 
-                alt="logo"
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEMmBA2kQGJt8ApVm5zWD_rZFJeFu3T2zH0w&usqp=CAU"
+                    alt="logo"
                 />
-                
+
 
             </div>
             <div className="header__middle">
                 <SearchIcon />
-                <input placeholder="search mail" type="text"/>
-                <ArrowDropDownIcon className="header__inputCarret"/>
+                <input placeholder="search mail" type="text" />
+                <ArrowDropDownIcon className="header__inputCarret" />
 
             </div>
             <div className="header__right">
                 <IconButton>
-                <AppsIcon/>
+                    <AppsIcon />
                 </IconButton>
                 <IconButton>
-                    <NotificationsIcon/>
+                    <NotificationsIcon />
                 </IconButton>
-                <Avatar/>
+                <Avatar onClick={signOut} src={user?.photoUrl}/>
             </div>
-            
+
         </div>
     )
 }
